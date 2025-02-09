15a86934e804e97aa09f01e4747b10f660cb8b2d
flashgnash <flashgnash@proton.me>
2025-02-09 16:37:57 +0000

    Nix flake for dev env (poetry2nix nonfunctional)

---
+++ flake.nix
@@ -0,0 +1,46 @@
+# file: flake.nix
+{
+  description = "Global Capslock";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      poetry2nix,
+    }:
+    let
+      system = "x86_64-linux";
+
+      pkgs = nixpkgs.legacyPackages.${system};
+      inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
+      pythonApp = mkPoetryApplication {
+        projectDir = ./.;
+        preferWheel = true;
+        nativeBuildInputs = (pythonApp.nativeBuildInputs or [ ]) ++ [ pkgs.python3Packages.setuptools ];
+      };
+    in
+    {
+      apps.${system}.default = {
+        type = "app";
+        program = "${pythonApp}/bin/global-capslock";
+
+      };
+
+      devShells.${system}.default = pkgs.mkShell {
+        name = "global-capslock";
+        packages = with pkgs; [
+          poetry
+          linuxHeaders
+        ];
+      };
+
+    };
+}
